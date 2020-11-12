5957bbdd85d9470da4307cbd75e31e05c35099e0
Russell <russelldev@protonmail.com>
2020-11-12 18:26:49 +0000

    make vars

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 //ask usr what action to perorm
-
+const   askUsr = prompt('Please select action:'),
+        createEntry = [];
+let new = 'new',
+    list = 'list',
+    delete = 'delete',
+    quit = 'q';
 //if new - add new item to arr
 
 //if list - list all to-dos
